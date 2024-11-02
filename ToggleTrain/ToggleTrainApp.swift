c9454af909af27515b43aea91940ac274e46058a
danyaDerdan <110595066+danyaDerdan@users.noreply.github.com>
2024-11-02 09:54:05 +0300

    Initial Commit

---
+++ ToggleTrain/ToggleTrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleTrainApp.swift
+//  ToggleTrain
+//
+//  Created by Данил Толстиков on 02.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToggleTrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ToggleTrainTests/ToggleTrainTests.swift
@@ -0,0 +1,17 @@
+//
+//  ToggleTrainTests.swift
+//  ToggleTrainTests
+//
+//  Created by Данил Толстиков on 02.11.2024.
+//
+
+import Testing
+@testable import ToggleTrain
+
+struct ToggleTrainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
